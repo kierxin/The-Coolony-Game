55aa94827adefe4437bc2430555372c8e14b5764
Kira Porter <kierxin@gmail.com>
2021-08-13 18:58:27 -0400

    Tweak janky drag scrolling

--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -6,11 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const draggable = document.getElementById("draggable");
 
 
+
+    // DRAGGABLE GAME GRID
     // draggable.addEventListener('drag', redize.bind(draggable));
     draggable.ondrag = calculateMouseMoveDistance.bind(gameGridContainer);
     draggable.ondragend = visuallyShiftGrid.bind(gameGridContainer);
 
-    let formerScrollPos = [];
     let scrollPositions = [];
 
     function calculateMouseMoveDistance(e) {
@@ -19,16 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function visuallyShiftGrid(e) {
-        this.scrollTop += (scrollPositions[1][1] - scrollPositions[scrollPositions.length - 2][1]) * 3;
-        this.scrollLeft += (scrollPositions[1][0] - scrollPositions[scrollPositions.length - 2][0]) *3;
-
-        console.log(this.scrollTop);
+        this.scrollTop += (scrollPositions[1][1] - scrollPositions[scrollPositions.length - 2][1]) * 5;
+        this.scrollLeft += (scrollPositions[1][0] - scrollPositions[scrollPositions.length - 2][0]) * 5;
     }
-
-    // function redize() {
-    //     console.log('yay');
-    //     gameGridContainer.style.color = 'red';
-    // }
 
 });
 
