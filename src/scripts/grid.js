a011c12854c4b9b1979fda4835aa5fc938ed4ab0
Kira Porter <kierxin@gmail.com>
2021-08-13 18:43:02 -0400

    Get game board background to be draggable - janky but it works

--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -1,23 +1,34 @@
-const gameGridContainer = document.querySelector(".game-grid-container");
-const gameGrid = document.querySelector(".game-grid");
+document.addEventListener('DOMContentLoaded', () => {
 
-// gameGridContainer.addEventListener('mousedown', );
-
-gameGrid.addEventListener('mousedown', myEvent.bind(gameGrid));
-
-
-
-function myEvent(e) {
-    startX = e.pageX;
-    startY = e.pageY;
-    console.log(this);
-}
+    // QUERY SELECTORS
+    const gameGridContainer = document.querySelector(".game-grid-container");
+    const gameGrid = document.querySelector(".game-grid");
+    const draggable = document.getElementById("draggable");
 
 
-// function textToRed(e) {
-//     return this.style.color="red";
-// }
+    // draggable.addEventListener('drag', redize.bind(draggable));
+    draggable.ondrag = calculateMouseMoveDistance.bind(gameGridContainer);
+    draggable.ondragend = visuallyShiftGrid.bind(gameGridContainer);
 
-// function textToPurple(e) {
-//     return this.style.color = "purple";
-// }
+    let formerScrollPos = [];
+    let scrollPositions = [];
+
+    function calculateMouseMoveDistance(e) {
+        scrollPositions.push([e.clientX, e.clientY]);
+        console.log(scrollPositions[scrollPositions.length - 2]);
+    }
+
+    function visuallyShiftGrid(e) {
+        this.scrollTop += (scrollPositions[1][1] - scrollPositions[scrollPositions.length - 2][1]) * 3;
+        this.scrollLeft += (scrollPositions[1][0] - scrollPositions[scrollPositions.length - 2][0]) *3;
+
+        console.log(this.scrollTop);
+    }
+
+    // function redize() {
+    //     console.log('yay');
+    //     gameGridContainer.style.color = 'red';
+    // }
+
+});
+
