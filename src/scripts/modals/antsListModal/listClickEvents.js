61f6a88f98036bca0da600f2dc593bd77ad85644
Kira Porter <kierxin@gmail.com>
2021-08-18 00:02:55 -0400

    Fix ant list toggling to allow user to click through form

--- src/scripts/modals/antsListModal/listClickEvents.js
+++ src/scripts/modals/antsListModal/listClickEvents.js
@@ -3,41 +3,23 @@ export function ListClickEvents () {
     const over = document.querySelectorAll(".over");
     const under = document.querySelectorAll(".under");
 
-    // console.log(over);
-    // console.log(over[0]);
-    // console.log(over[1]);
-    // console.log(over[2]);
-    // console.log(under[0]);
-    // console.log(under[1]);
-    // console.log(under[2]);
-
-    // over.forEach(el => console.log(el));
 
     for (let i = 0; i < over.length; i++) {
-
-        // console.log(over[i]);
-        // console.log(under)
         over[i].addEventListener("click", overListener);
 
         function overListener() {
-            console.log("hi");
-            // console.log(under[i]);
-            over[i].removeEventListener("click", overListener);
-            under[i].addEventListener("click", underListener);
-            over[i].style.opacity = 0;
-            over[i].style.pointerEvents = "none";
+            
+            if (under[i].getAttribute("display") === "block") {
+                under[i].style.display = "none";
+                under[i].setAttribute("display", "none");
+            } else {
+                under[i].style.display = "block";
+                under[i].setAttribute("display", "block");
+                console.log(under[i].getAttribute("display"));
+            }
+
             under[i].style.opacity = 1;
             under[i].style.pointerEvents = "auto";
-        }
-
-        function underListener() {
-            console.log("bye");
-            under[i].removeEventListener("click", underListener);
-            over[i].addEventListener("click", overListener);
-            under[i].style.opacity = 0;
-            under[i].style.pointerEvents = "none";
-            over[i].style.opacity = 1;
-            over[i].style.pointerEvents = "auto";
         }
     }
 
